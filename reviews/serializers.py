411f2556ac4f7c6294427498d9d0163550dcd351
Manoel Cândido <candidomanoel78@gmail.com>
2024-06-06 09:47:22 -0300

    adjustments to the reviews serializers

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -12,3 +12,14 @@ class ReviewSerializer(serializers.ModelSerializer):
         if len(value) > 200:
             raise serializers.ValidationError('O seu comentário não pode exceder 200 caracteres.')
         return value
+
+
+class ReviewListDetailSerializer(serializers.ModelSerializer):
+    movie_title = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Review
+        fields = ['id', 'movie_title', 'stars', 'comment']
+
+    def get_movie_title(self, obj):
+        return obj.movie.title
--- reviews/views.py
+++ reviews/views.py
@@ -2,13 +2,17 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermissionClass
 from reviews.models import Review
-from reviews.serializers import ReviewSerializer
+from reviews.serializers import ReviewSerializer, ReviewListDetailSerializer
 
 
 class ReviewCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermissionClass,)
     queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ReviewListDetailSerializer
+        return ReviewSerializer
 
 
 class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
